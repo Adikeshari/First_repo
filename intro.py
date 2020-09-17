285e28a08478b2eca49a32886a215e64c5004628
unknown <SWAT@DESKTOP-HBLF2DV>
2020-09-17 19:42:28 +0530

    initial commit

---
+++ intro.py
@@ -0,0 +1,16 @@
+print('hey i am working with git')
+print('its my first code')
+#pattern code
+'''
+*
+**
+***
+****
+*****
+'''
+num=int(input())+1
+for i in range(num):
+    for j in range(i):
+        print('*',end='')
+    print()
+        
